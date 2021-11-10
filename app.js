26321bd48b36f930c684e13b9544e7b0aed40c3c
gonzalopez-ml <gonzalo.glopez@mercadolibre.com>
2021-11-10 19:54:30 -0300

    add

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = 3000;
 
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath)); //le dice a express que public es un recurso estatico
@@ -9,6 +10,6 @@ app.get("/home", function (req, res) {
   res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || port, () => {
   console.log("Servidor corriendo en puerto 3000");
 });
